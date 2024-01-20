84bc58c5e1cb81e2e414e9d55c0af9d123bf98b7
7omahawk <ahulr.official@gmail.com>
2024-01-21 02:40:08 +0600

    add percentage functionality with some modification

--- calculator.c
+++ calculator.c
@@ -6,32 +6,63 @@
 
 int main()
 {
-    int num1, num2, sum, sub, mul;
+    int select;
+    float num1, num2, sum, sub, mul, percentage;
     float div;
     char ch;
 
-    printf("Enter two number to calculate(+,-,*,/): ");
-    scanf("%d%c%d", &num1, &ch, &num2);
+    printf("\nFor calculate percentage select YES or NO [YES = 1, NO = 2]: ");
+    scanf("%d", &select);
 
-    if (ch == '+')
+    switch (select)
     {
-        sum = num1 + num2;
-        printf("%d %c %d = %d", num1, ch, num2, sum);
-    }
-    else if (ch == '-')
-    {
-        sub = num1 - num2;
-        printf("%d %c %d = %d", num1, ch, num2, sub);
-    }
-    else if (ch == '*')
-    {
-        mul = num1 * num2;
-        printf("%d %c %d = %d", num1, ch, num2, mul);
-    }
-    else if (ch == '/')
-    {
-        div = (float)num1 / num2;
-        printf("%d %c %d = %.2f", num1, ch, num2, div);
+        // here the code of %
+        case 1:
+        {
+            printf("\nEnter the number for checking percentage [ex: 10%%]: ");
+            scanf("%f",&num1);
+            percentage = num1 / 100;
+            printf("\nThe percentage of %.2f%% is: %.2f ", num1, percentage);
+            break;
+        }
+
+        // here the code of +,-,*,/
+        case 2:
+        {
+            printf("\nEnter two number to calculate(+,-,*,/ or %%): ");
+            scanf("%f%c%f", &num1, &ch, &num2);
+
+            if (ch == '+')
+            {
+                //addition
+                sum = num1 + num2;
+                printf("%.2f %c %.2f = %.2f", num1, ch, num2, sum);
+            }
+            else if (ch == '-')
+            {
+                //subtraction
+                sub = num1 - num2;
+                printf("%.2f %c %.2f = %.2f", num1, ch, num2, sub);
+            }
+            else if (ch == '*')
+            {
+                //multiplication
+                mul = num1 * num2;
+                printf("%.2f %c %.2f = %.2f", num1, ch, num2, mul);
+            }
+            else if (ch == '/')
+            {
+                //division
+                div = num1 / num2;
+                printf("%.2f %c %.2f = %.2f", num1, ch, num2, div);
+            }
+            break;
+        }
+
+        default:
+        {
+            printf("\nError.");
+        }
     }
 
     return 0;
